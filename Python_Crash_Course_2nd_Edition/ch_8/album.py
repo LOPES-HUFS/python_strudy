6700dab62df8478d0e3483bd9ac9bfe459e35039
Jaehwan <dialektike@gmail.com>
2022-05-14 18:10:01 +0900

    black album.py

--- Python_Crash_Course_2nd_Edition/ch_8/album.py
+++ Python_Crash_Course_2nd_Edition/ch_8/album.py
@@ -1,14 +1,15 @@
 def make_album(artist, title, song_numbers=None):
     album = {"artist": artist, "title": title}
     if song_numbers:
-        album['song_numbers'] = song_numbers
+        album["song_numbers"] = song_numbers
     return album
 
-musician = make_album('산울림', '산울림 5집', song_numbers=13)
+
+musician = make_album("산울림", "산울림 5집", song_numbers=13)
 print(musician)
 
-musician = make_album('서태지', '산울림 1집')
+musician = make_album("서태지", "산울림 1집")
 print(musician)
 
-musician = make_album('Psy', '싸이 1집')
+musician = make_album("Psy", "싸이 1집")
 print(musician)
