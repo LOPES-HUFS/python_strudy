d98c335a3e7a98f04c4d094f7c4b72e94aed9729
Jaehwan <dialektike@gmail.com>
2022-05-14 18:11:20 +0900

    black printing_models.py

--- Python_Crash_Course_2nd_Edition/ch_8/printing_models.py
+++ Python_Crash_Course_2nd_Edition/ch_8/printing_models.py
@@ -13,19 +13,22 @@
 
 print("\n함수로 만든 것 실행하기")
 
+
 def print_models(unprinted_designs, completed_models):
-    while unprinted_designs :
+    while unprinted_designs:
         print(f"사본:{unprinted_designs}")
         current_design = unprinted_designs.pop()
         print(f"Printing model:{current_design}")
         completed_models.append(current_design)
+
 
 def show_completed_model(completed_model):
     print("\nThe following models have been printed:")
     for completed_model in completed_models:
         print(completed_model)
 
-unprinted_designs = ['phone case', 'robot pendant', 'dodecaheron']
+
+unprinted_designs = ["phone case", "robot pendant", "dodecaheron"]
 completed_models = []
 
 print_models(unprinted_designs[:], completed_models)
