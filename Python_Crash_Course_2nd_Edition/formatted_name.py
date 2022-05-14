c7bb35e50656790c072dd8cfaedcd0feaec14c03
Jaehwan <dialektike@gmail.com>
2022-05-14 16:32:42 +0900

    8.3.1 까지

---
+++ Python_Crash_Course_2nd_Edition/formatted_name.py
@@ -0,0 +1,9 @@
+from asyncio import futures
+
+
+def get_formatted_name(first_name, last_name):
+    full_name = f"{first_name} {last_name}"
+    return full_name
+
+musician = get_formatted_name('jimi', 'hendrix')
+print(musician)
